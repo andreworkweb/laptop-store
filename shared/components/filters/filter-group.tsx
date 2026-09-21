506f717f696374eb640f474f75f306f0a7d7f6cc
Andrew <andreworkweb@gmail.com>
2026-09-21 19:19:26 +0300

    feat: laptop configuration price calculation

--- shared/components/filters/filter-group.tsx
+++ shared/components/filters/filter-group.tsx
@@ -1,5 +1,4 @@
-import { Cpu, HardDrive } from "lucide-react";
-import { ReactNode } from "react";
+import { useState } from "react";
 
 interface Filter {
   id: string;
@@ -11,6 +10,7 @@ interface Filter {
 interface FilterItem {
   label: string;
   value: string;
+  price: number;
 }
 
 export const filtersData: Filter[] = [
@@ -19,10 +19,10 @@ export const filtersData: Filter[] = [
     title: "RAM",
     defaultValue: "16",
     items: [
-      { label: "8 GB", value: "8" },
-      { label: "16 GB", value: "16" },
-      { label: "32 GB", value: "32" },
-      { label: "64 GB", value: "64" },
+      { label: "8 GB", value: "8", price: -100 },
+      { label: "16 GB", value: "16", price: 0 },
+      { label: "32 GB", value: "32", price: 100 },
+      { label: "64 GB", value: "64", price: 250 },
     ],
   },
   {
@@ -30,10 +30,10 @@ export const filtersData: Filter[] = [
     title: "SSD Storage",
     defaultValue: "512",
     items: [
-      { label: "256 GB", value: "256" },
-      { label: "512 GB", value: "512" },
-      { label: "1 TB", value: "1024" },
-      { label: "2 TB", value: "2048" },
+      { label: "256 GB", value: "256", price: -50 },
+      { label: "512 GB", value: "512", price: 0 },
+      { label: "1 TB", value: "1024", price: 100 },
+      { label: "2 TB", value: "2048", price: 250 },
     ],
   },
   {
@@ -41,15 +41,36 @@ export const filtersData: Filter[] = [
     title: "Processor",
     defaultValue: "M2",
     items: [
-      { label: "Galaxy M1", value: "M1" },
-      { label: "Galaxy M2", value: "M2" },
-      { label: "Galaxy M3", value: "M3" },
-      { label: "Galaxy M4", value: "M4" },
+      { label: "Galaxy M1", value: "M1", price: -101 },
+      { label: "Galaxy M2", value: "M2", price: 0 },
+      { label: "Galaxy M3", value: "M3", price: 260 },
+      { label: "Galaxy M4", value: "M4", price: 500 },
     ],
   },
 ];
 
 export const FilterGroup = () => {
+  const LAPTOP_PRICE = 1100;
+
+  const [selectedValue, setSelectedValue] = useState<Record<string, number>>(
+    {},
+  );
+
+  const handleSet = (filterId: string, price: number) => {
+    setSelectedValue((prev) => ({
+      ...prev,
+      [filterId]: price,
+    }));
+  };
+
+  const priceAccum = Object.values(selectedValue).reduce((sum, price) => {
+    return sum + price;
+  }, 0);
+
+  const totalPrice = () => {
+    return LAPTOP_PRICE + priceAccum;
+  };
+
   return (
     <div className="flex flex-col gap-6">
       {filtersData.map((filter) => (
@@ -78,16 +99,27 @@ export const FilterGroup = () => {
                   type="radio"
                   name={filter.id}
                   value={item.value}
-                  defaultChecked={item.value === filter.defaultValue}
                   className="sr-only"
+                  onChange={() => handleSet(filter.id, item.price)}
+                  defaultChecked={item.value === filter.defaultValue}
                 />
 
-                <p className="font-semibold">{item.label}</p>
+                <span className="flex flex-col items-center gap-0.5">
+                  <span className="font-semibold">{item.label}</span>
+                  <span className="text-[10px] opacity-70">
+                    {item.price === 0 ? "Included" : item.price}
+                  </span>
+                </span>
               </label>
             ))}
           </div>
         </div>
       ))}
+      <div className="mt-5 flex items-center justify-between text-[#100E09]">
+        <p className="text-xl font-semibold">Total price:</p>
+
+        <p className="text-3xl font-bold tracking-tight">${totalPrice()}</p>
+      </div>
     </div>
   );
 };
